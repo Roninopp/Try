e28714d45bfb4d4e9bfe13a51b219f832a6d6360
VIVEK <99870677+Roninopp@users.noreply.github.com>
2025-12-07 20:58:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # config.py
 
 # --- Telegram Bot Configuration ---
-API_ID = 1234567          # Your Telegram API ID (Use the SAME one for both Bot and User)
-API_HASH = "YOUR_API_HASH"  # Your Telegram API Hash (Use the SAME one for both Bot and User)
-BOT_TOKEN = "YOUR_BOT_TOKEN" # Your Bot's Token from BotFather
+API_ID = 21502134          # Your Telegram API ID (Use the SAME one for both Bot and User)
+API_HASH = "e09a3f453b841ca4d1823d3b4004672d"  # Your Telegram API Hash (Use the SAME one for both Bot and User)
+BOT_TOKEN = "8341511264:AAFjNIOYE5NbABPloFbz-r989l2ySRUs988" # Your Bot's Token from BotFather
 
 # --- UserBot Configuration (REQUIRED for Voice Chat) ---
 # THIS IS YOUR ACCOUNT'S SESSION STRING!
 # You must generate this using the script below.
-USER_SESSION_STRING = "" # <-- PASTE THE GENERATED STRING HERE
+USER_SESSION_STRING = "BQFIGLYAqikS4rDDZuGJ5LCf77uHUX1QbHvQBz6JMb_nRuZMf47HvOcd_CYm2Fpplfw25_SZ05iK7mwlRq4pMqo7Mgn-Fus-4rIkcvnD5TtztrfSWccfGUnnxa7KwsbsQEIXXDw2r3sO-jL4D-eJKQlCF1K2SANN0du5yDWoBDfTbj6T45qxMKElYFNVq1eJJXc6NwdFfyA239k7T46vOPq7vhRyzR7MoVzoXt7MYUrlNncLS4hfphDsBCnUAElDUo5y-V9TJRcMsnNt-VnO4mRtVoWrSOb1S8G3djlq3K-sjQlQnFDjEVwh0DvVSw4lc0D2uYmJy99HBBAyJ7GZbVLH-DQ1ZQAAAAHEV5BpAA" # <-- PASTE THE GENERATED STRING HERE
 
 # --- Proxy Server Configuration ---
 PROXY_HOST_IP = "127.0.0.1" 
